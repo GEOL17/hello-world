8e475ba87955b7dd6ce0edc9268b2b2cb2698551
Gerardo Olivares <gerardo.olivaresn@gmail.com>
2020-11-10 14:27:26 -0600

    First version of Bienvenido.java from Java como programar by Deitel & Deitel

---
+++ Bienvenido.java
@@ -0,0 +1,11 @@
+// Fig. 2.1: Bienvenido.java
+// Programa para imprimir texto.
+
+public class Bienvenido
+{
+   // el método main empieza la ejecución de la aplicación en Java
+   public static void main( String[] args )
+   {
+      System.out.println( "Bienvenido a la programación en Java!" );
+   } // fin del método main 
+} //fin de la clase Bienvenido
